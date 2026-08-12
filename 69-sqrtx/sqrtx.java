cb493a6a4ef11b9c31ed842b7d40a4f6ee963a3c
ADI032912 <adityatiwari121007@gmail.com>
2026-08-12 12:45:12 +0530

    Time: 1 ms (97.94%) | Memory: 42.8 MB (23.79%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        int l = 1;
+        int r = x;
+        while(l<=r){
+            int m = l +((r-l)/2);
+            if(m==(x/m)){
+                return m;
+            }
+            else if(m<(x/m)){
+                l = m+1;
+            }
+            else{
+                r = m-1;
+            }
+        }
+        return r;
+    }
+}
