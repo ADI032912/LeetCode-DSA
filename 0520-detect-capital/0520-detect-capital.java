89f196b6f1c1fe1b2585e052de982926dac6d3f3
Aditya Tiwari <adityatiwari121007@gmail.com>
2026-09-23 15:30:16 +0530

    Time: 1 ms (89.17%), Space: 43.1 MB (61.88%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int capital = 0;
+        for(int i = 0;i<word.length();i++){
+            if(Character.isUpperCase(word.charAt(i))){
+                capital++;
+            }
+        }
+        if(word.length()==capital){
+            return true;
+        }
+        if(capital==0){
+            return true;
+        }
+        if(capital==1&&Character.isUpperCase(word.charAt(0))){
+            return true;
+        }
+        return false;
+    }
+}
